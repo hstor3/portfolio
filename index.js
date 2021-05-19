fbc7a83b0ccd596e337563170432e134bb29af18
Heather Storseth <heatherstorseth3@gmail.com>
2021-05-18 22:18:49 -0400

    gsap incorporation

---
+++ index.js
@@ -0,0 +1,59 @@
+gsap.fromTo(
+    '#js',
+    { opacity: 0 },
+    { delay: 0, opacity: 1, x: 1000, duration: 2.7 }
+);
+
+gsap.fromTo(
+    '#html',
+    { opacity: 0 },
+    { delay: 0.2, opacity: 1, x: 1000, duration: 2.7 }
+);
+
+gsap.fromTo(
+    '#css',
+    { opacity: 0 },
+    { delay: 0.4, opacity: 1, x: 1000, duration: 2.7 }
+);
+
+gsap.fromTo(
+    '#node',
+    { opacity: 0 },
+    { delay: 0.6, opacity: 1, x: 1000, duration: 2.7 }
+);
+
+gsap.fromTo(
+    '#react',
+    { opacity: 0 },
+    { delay: 0.8, opacity: 1, x: 1000, duration: 2.7 }
+);
+
+gsap.fromTo(
+    '#data',
+    { opacity: 0 },
+    { delay: 1, opacity: 1, x: 1000, duration: 2.7 }
+);
+
+gsap.fromTo(
+    '#boots', 
+    { opacity: 0 },
+    { delay: 1.2, opacity: 1, x: 1000, duration: 2.7 }
+);
+
+gsap.fromTo(
+    '#plus',
+    { opacity: 0 },
+    { delay: 1.4, opacity: 1, x: 1000, duration: 4, rotation: 1080 }
+);
+
+gsap.fromTo(
+    '#snowplow',
+    { opacity: 0 },
+    { delay: 1.6, opacity: 1, x: 1000, duration: 4.3 }
+);
+
+// gsap.fromTo(
+//     '#snowplow',
+//     { opacity: 1, delay: 3.8 },
+//     { opacity: 0, delay: 7.1 }
+// )
